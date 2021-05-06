7d2fc9fe2b8c439e911a363c7bce7c4e189457ee
Hsnjim <jimhessanon@gmail.com>
2021-05-06 12:53:14 +0100

    modif2_jim

--- app/src/main/java/com/example/expensio/MainActivity.java
+++ app/src/main/java/com/example/expensio/MainActivity.java
@@ -5,24 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int SPLASH_SCREEN = 5000;
-
+    Animation anim;
+    ImageView logo_expensio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new Handler().postDelayed(new Runnable() {
+        logo_expensio=(ImageView)findViewById(R.id.logo_expensio); // Declare an imageView to show the animation.
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade); // Create the animation.
+        anim.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void run() {
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
                 Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(intent));
                 finish();
                 return;
+                // HomeActivity.class is the activity to go after showing the splash screen.
             }
-        }, SPLASH_SCREEN);
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        logo_expensio.startAnimation(anim);
     }
 }
