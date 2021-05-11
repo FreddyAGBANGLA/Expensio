80599e1e6b38a6786ea07dedcd989342f2a71791
Hsnjim <jimhessanon@gmail.com>
2021-05-11 11:43:18 +0100

    modif_du_11_mai_2021_11h_42

--- app/src/main/java/com/example/expensio/AccueilFragment.java
+++ app/src/main/java/com/example/expensio/AccueilFragment.java
@@ -1,21 +1,35 @@
 package com.example.expensio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class AccueilFragment extends Fragment {
+    ImageButton B;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_accueil, container, false);
-    }
+                             @Nullable Bundle savedInstanceState)
+        {
+            View view = inflater.inflate(R.layout.fragment_accueil, container, false);
+            B = view.findViewById(R.id.button_revenu);
+
+            B.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getContext(),RevenusActivity.class);
+                    startActivity(intent);
+                }
+            });
+            return view;
+        }
 }
